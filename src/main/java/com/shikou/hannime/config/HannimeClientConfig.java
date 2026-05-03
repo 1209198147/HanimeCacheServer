2109ad5f79c86cc024ec2e8660e6ab980ee3c826
Shikou <1209198147@qq.com>
2026-05-03 20:28:32 +0800

    支持简体中文源

--- src/main/java/com/shikou/hannime/config/HannimeClientConfig.java
+++ src/main/java/com/shikou/hannime/config/HannimeClientConfig.java
@@ -11,16 +11,23 @@ public class HannimeClientConfig {
     /** 自定义Base URL */
     @Value("${hannime.zone:HK}")
     private String zone;
+    @Value("${hannime.lang:zhs}")
+    private String lang;
 
     @Bean
     public HanimeApiClient hanimeApiClient() {
+        HanimeConfig config = null;
         if ("HK".equalsIgnoreCase(zone)) {
-            return HanimeApiClient.builder()
-                    .config(HanimeConfig.defaultConfigHK())
+            config = HanimeConfig.builder()
+                    .baseUrl(HanimeConfig.DEFAULT_BASE_URL_HK)
+                    .userLang(lang)
+                    .build();
+        }else{
+            config = HanimeConfig.builder()
+                    .baseUrl(HanimeConfig.DEFAULT_BASE_URL)
+                    .userLang(lang)
                     .build();
         }
-        return HanimeApiClient.builder()
-                .config(HanimeConfig.defaultConfig())
-                .build();
+        return HanimeApiClient.builder().config(config).build();
     }
 }
